5042c84bbaac079c04820c63397528ae7a0c4f08
Henry Bryan <Kamp@Bryans-MacBook-Air-2.local>
2016-07-24 07:51:57 -0500

    added new folders to project structure

---
+++ src/Objects/Game.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Objects;
+
+/**
+ * @author Bryan Wodi <talk2kamp@gmail.com>
+ *
+ */
+public class Game {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
