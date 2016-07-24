9e9371413aa73543d028a81c72fc31c05777a1b2
Bryan Wodi <talk2kamp@gmail.com>
2016-07-24 09:33:53 -0500

    added game rules in text file

---
+++ src/presentation/ForAlyssa.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package presentation;
+
+import java.sql.Date;
+
+/**
+ * @author Bryan Wodi <talk2kamp@gmail.com>
+ *
+ */
+import objects.TheGame;
+public class ForAlyssa {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Date myDate;
+	    TheGame game = new TheGame();
+	    myDate = new Date(0);
+	    System.out.println("Sefini");
+	    System.out.println(myDate);
+
+	}
+
+}
