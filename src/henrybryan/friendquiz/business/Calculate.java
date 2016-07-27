0d695f5e59cbf407ac1727c6719ff11afdc807d5
Bryan Wodi <talk2kamp@gmail.com>
2016-07-27 16:39:23 -0500

    stable! more design needs to be implemented though

--- src/henrybryan/friendquiz/business/Calculate.java
+++ src/henrybryan/friendquiz/business/Calculate.java
@@ -9,7 +9,7 @@ package henrybryan.friendquiz.business;
  */
 public class Calculate {
 	
-	public static double claculatePercentage(int totalScore, int maxScore) {
+	public static double claculatePercentage(double totalScore, int maxScore) {
 		double percent = 0.0;
 		percent = (double)Math.round(((totalScore/maxScore)*100.0)*100)/100;
 		return percent;
--- src/henrybryan/friendquiz/presentation/SimulateGame.java
+++ src/henrybryan/friendquiz/presentation/SimulateGame.java
@@ -4,11 +4,13 @@
 package henrybryan.friendquiz.presentation;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
 import henrybryan.friendquiz.business.AccessQuestions;
+import henrybryan.friendquiz.business.Calculate;
 import henrybryan.friendquiz.objects.Question;
 import henrybryan.friendquiz.objects.UserResponse;
 
@@ -22,7 +24,7 @@ public class SimulateGame {
 	  private ArrayList<Question> questions;
 	  private ArrayList<UserResponse> responses;
 	  private double totalScore;
-	  private double maxScore;
+	  private int maxScore;
 	  private double percent;
 	  private String player;
 	  private int count;
@@ -34,10 +36,21 @@ public class SimulateGame {
 		responses = new ArrayList<>();
 		currentIndex = 0;
 		kbd = new Scanner(System.in);
+		totalScore = 0.0;
+		percent = 0.0;
+		setMaxScore();
 	}
 	
+	private void setMaxScore() {
+		maxScore = 0;
+		for (Question question : questions) {
+			maxScore += question.getPassMark();
+		}
+	}
+
 	public void playGame(){
 		String userResponse;
+		
 		for (Question question : questions){
 			System.out.println(question.getQuestion());
 			userResponse = kbd.nextLine();
@@ -45,15 +58,54 @@ public class SimulateGame {
 			validateResponse(userResponse);
 			currentIndex++;
 		}
+		displaySummary();
 	}
 	
+	private void displaySummary() {
+		percent = getPercentage();
+		
+		System.out.println("\n***************RESULT***************\n");
+		System.out.println("Your score is: "+ totalScore+" out of "+maxScore+"\n");
+		System.out.println("In terms of percentage, you have scored : "+ percent+"%\n");
+		System.out.println(""+((percent<=70)? "You are a bad friend "+player+" :(":"You are a good friend "+player+" :)"));
+		
+		System. out.println("\n\nQuestions were designed and edited by Aleli(80%) and Bryan(20%)\n");
+		System.out.println("Programmed by Bryan Wodi, Mar 9, 2015. For Alyssa, for being such a good friend\n");
+		System.out.println("We all love you Alyssa\n");
+		System.out.println("Courtesy: Aleli & Bryan, on behalf of Walter, Richard, Tobi, Joshua and Nguvan\n");
+		System.out.println("\n\nLong live our friendship\n");
+		System.out.println("\n\n***************THE END****************\n");
+		
+		//JOptionPane.showMessageDialog(null,"Check out your tanscripts in \"log.txt\" located in this parent directory");
+		
+	}
+
+	private double getPercentage() {
+		
+		return Calculate.claculatePercentage(totalScore, maxScore);
+	}
+
 	private void validateResponse(String userResponse) {
 		Question question = questions.get(currentIndex);
+		UserResponse response = new UserResponse(userResponse);
+		responses.add(currentIndex, response);
+		
 		if (question.getKey().equalsIgnoreCase(userResponse)) {
+			updateScore(question,true);
 			System.out.println("You are correct");
 		} else {
+			updateScore(question, false);
 			System.out.println("uhh! you're wrong");
 		}
+	}
+
+	private void updateScore(Question question, boolean b) {
+		if (b) {
+			totalScore += question.getPassMark();
+		}else {
+			totalScore += question.getFailMark();
+		}
+		
 	}
 
 }
